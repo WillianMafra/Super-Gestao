09bff4b9aa19b7c2867bb9599ff3925a83b38c29
Willian <williannunes965@gmail.com>
2024-02-21 13:09:52 -0300

    salvando enviado contato via form post

--- resources/views/site/layouts/_components/form_contato.blade.php
+++ resources/views/site/layouts/_components/form_contato.blade.php
@@ -9,9 +9,9 @@
     <br>
     <select name="motivo_contato" class="borda-preta">
         <option value="">Qual o motivo do contato?</option>
-        <option value="">Dúvida</option>
-        <option value="">Elogio</option>
-        <option value="">Reclamação</option>
+        <option value="1">Dúvida</option>
+        <option value="2">Elogio</option>
+        <option value="3">Reclamação</option>
     </select>
     <br>
     <textarea name="mensagem" class="borda-preta">Preencha aqui a sua mensagem</textarea>
