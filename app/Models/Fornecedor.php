e2dc2ce1a2e766d8d7ba3c09dbcbd40a63c74590
Willian <williannunes965@gmail.com>
2024-02-26 22:30:45 -0300

    item e item detalhe

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Fornecedor extends Model
 {
     protected $table = 'fornecedores';
     protected $fillable = ['nome', 'site', 'estado_id', 'email']; // Para tornar acessivel a criação via metodo create no tinker \App\Models\Fornecedor::create(arrayDados)
     use HasFactory;
+
+    public function produtos(): HasMany
+{
+    $this->hasMany(Produto::class)
+}
 }
